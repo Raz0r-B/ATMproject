7660c04d6856fb200c42eac26583c157ea7e6ea0
Mech <Mech@DESKTOP-IDG0P90>
2021-07-18 15:02:03 -0400

    Added MySQL Integration. New Customers will now be added to the MySQL database.

---
+++ ATM/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM
+{
+    class Customer
+    {
+        public string fName;
+        public string lName;
+        public string email;
+        public int pin;
+
+        public Customer(string AfName, string AlName, string Aemail, int Apin ) //The 'A' denotes the argument 
+        {
+            fName = AfName;
+            lName = AlName;
+            email = Aemail;
+            pin = Apin;
+
+        }
+
+
+    }
+}
---
+++ ATM/Program.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Linq; //Language integrated query, specifically for .ALL in makeNewCustomer() to insure proper input
+using MySql.Data.MySqlClient;
+
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            // Customer customer0 = new Customer("Brandon", "scott", "myemail@email.com", 12345);
+            //Console.WriteLine(customer0.fName);
+
+
+
+
+            WelcomeScreen();
+
+           
+
+            //YOU DID IT YOU MADE A DAMN CONNECTION!
+            //NEXT WE GOTTA TEST THAT IT IS ACTUALLY CONNECTING TO THE DESIRED DATABASE
+
+
+       
+           
+
+
+        }
+
+        //TODO - Create method that builds a new customer profile ----I think this is completed
+        //TODO - Create method that passes the customer profile into MySQL DB
+        //TODO - Return Acct Number back to customer from SQL DB, opens new window to simulate an email or something like that
+
+        public static Customer MakeNewCustomer()
+        {
+
+
+            /*          This method is designed to ask the user to input details to make them a new customer
+             *          It is very much not secure.
+             *          
+             *          TODO: Make More Secure
+             *          TODO: Send input info to the SQL database
+             *          TODO: Launch new screen showing account number
+             * 
+             */
+
+            Console.WriteLine("Welcome to the New Customer Page!");
+            
+            //A pause for aesthetics
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(".");
+                System.Threading.Thread.Sleep(500);
+
+            }
+
+            int pin = 0;
+            string fName = "", lName = "", email = "";
+            bool check1 = true, check2 = true, check3 = true, check4 = true;
+            // First Name Check
+            while (check1 == true)
+            {
+
+                    Console.Write("Please Enter First Name: ");
+                    fName = Console.ReadLine();
+
+                if (fName.All(char.IsLetter))
+                {
+                    break;
+
+                }
+                else
+                {
+                    Console.WriteLine("A name can only containes letter!");
+                }
+ 
+               
+            }
+
+            // Last Name Check
+            while (check2 == true)
+            {
+
+                Console.Write("Please Enter Last Name: ");
+                lName = Console.ReadLine();
+
+                if (lName.All(char.IsLetter))
+                {
+                    break;
+
+                }
+                else
+                {
+                    Console.WriteLine("A name can only containes letter!");
+                }
+
+
+            }
+
+            // Email Check
+            while (check3 == true)
+            {
+
+                Console.Write("Please Enter Email: ");
+                email = Console.ReadLine();
+
+                if (email.Contains('@'))
+                {
+                    break;
+
+                }
+                else
+                {
+                    Console.WriteLine("Email must contain '@'");
+                }
+
+
+            }
+
+            while (check4 == true)
+            {
+                try
+                {
+                    Console.Write("Please Enter Your Desired PIN: ");
+                    pin = Convert.ToInt32(Console.ReadLine());
+                    break;
+
+                }
+
+                catch
+                {
+                    Console.WriteLine("PIN can only contain numbers");
+                    
+                }
+            }
+
+
+
+    Customer newCustomer = new Customer(fName, lName, email, pin);
+            CommitNewCustomer(newCustomer);
+            Console.WriteLine("\n\nWelcome {0} {1}!\n Your Account Number will be sent to {2}\n\n", newCustomer.fName, newCustomer.lName, newCustomer.email);
+
+
+            return newCustomer;
+            
+        }
+    
+        public static void WelcomeScreen()
+        {
+            /*
+             * 
+             *      This Method works as the welcome screen. It is the starting place and points to the next part of the ATM processes
+             *      This method is not secure
+             *      
+             *      
+             */
+            
+            // Get Input
+            Console.WriteLine("Welcome to the Bank of Brandon ATM");
+            Console.WriteLine("Please Input your account Number");
+            Console.WriteLine("If you are a new customer, type 'New'");
+            string acctNumOrNew = Console.ReadLine();
+            //Test Input
+            switch (acctNumOrNew)
+            {
+                case "New":
+                case "new":
+                    MakeNewCustomer();
+                    break;
+
+                default:
+                    Console.WriteLine("Validating...");
+                    System.Threading.Thread.Sleep(1000);
+                    Console.WriteLine("Validating...");
+                    System.Threading.Thread.Sleep(1000);
+                    Console.WriteLine("System Down\n\n");
+                    break;
+                    // TODO make this actually test account number against sql table 'account'
+
+            }
+            WelcomeScreen();
+
+
+        }
+
+
+        public static MySqlConnection SQLAccountNumCheck()
+        {
+            //Connection String
+            string connString = @"server=localhost;userid=root;password=password;database=test_atm";
+            using var con = new MySqlConnection(connString);
+
+
+            try // Statement wrapped in a try/Catch in case something goes wrong
+            {
+
+            //Opens the connection
+            con.Open();
+            Console.WriteLine("Connecting...");
+
+                //SQL Command goes here 
+                string sql = "SELECT * FROM CUSTOMERS WHERE account_number=@acct_num";
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+
+                //Get parameter to pass to sql query
+                Console.Write("\nEnter your account number: ");
+                int acctNum = Convert.ToInt32(Console.ReadLine());
+               
+                
+                    //pass to query
+                    cmd.Parameters.AddWithValue("@acct_num", acctNum);
+
+                    //Read out return
+                    MySqlDataReader rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        Console.WriteLine("Acct_num ---- First Name --- Last Name --- Email -- Acct Opened --- Pin");
+                        Console.WriteLine(rdr[0] + "---" + rdr[1] + "---" + rdr[2] + "---" + rdr[3] + "---" + rdr[4] + "---" + rdr[5]);
+
+                    }
+                    rdr.Close();
+
+               
+
+                
+
+
+                    
+            
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+
+            //Close Connections
+            con.Close();
+            Console.WriteLine("Done\nConnection Closed");
+            return con;
+
+        }
+
+        public static MySqlConnection CommitNewCustomer(Customer newCustomer)
+        {
+            //Connection String
+            string connString = @"server=localhost;userid=root;password=password;database=test_atm";
+            using var con = new MySqlConnection(connString);
+
+
+            try // Statement wrapped in a try/Catch in case something goes wrong
+            {
+
+                //Opens the connection
+                con.Open();
+                Console.WriteLine("Connecting...");
+
+
+                string addCust = "INSERT INTO customers (fName, lName, email, pin_num) VALUES (@fName, @lName, @email, @pin_num)";
+
+                MySqlCommand cmd = new MySqlCommand(addCust, con);
+
+                cmd.Parameters.AddWithValue("@fName", newCustomer.fName);
+                cmd.Parameters.AddWithValue("@lName", newCustomer.lName);
+                cmd.Parameters.AddWithValue("@email", newCustomer.email);
+                cmd.Parameters.AddWithValue("@pin_num", newCustomer.pin);
+                cmd.ExecuteNonQuery();
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+
+            //Close Connections
+            con.Close();
+            Console.WriteLine("Done\nConnection Closed");
+            return con;
+
+        }
+
+
+
+    }
+
+
+
+
+    
+}
+
+
